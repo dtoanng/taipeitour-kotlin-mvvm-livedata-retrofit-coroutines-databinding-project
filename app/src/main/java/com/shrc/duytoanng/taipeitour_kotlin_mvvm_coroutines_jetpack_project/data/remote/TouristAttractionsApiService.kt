eebd098914b128b818db58baa893b7e1e6f948eb
Nguyen Duy Toan <duytoan.ng@samsung.com>
2024-05-09 16:52:19 +0700

    chore: modify api, hilt injection

--- app/src/main/java/com/shrc/duytoanng/taipeitour_kotlin_mvvm_coroutines_jetpack_project/data/remote/TouristAttractionsApiService.kt
+++ app/src/main/java/com/shrc/duytoanng/taipeitour_kotlin_mvvm_coroutines_jetpack_project/data/remote/TouristAttractionsApiService.kt
@@ -1,5 +1,6 @@
 package com.shrc.duytoanng.taipeitour_kotlin_mvvm_coroutines_jetpack_project.data.remote
 
+import com.shrc.duytoanng.taipeitour_kotlin_mvvm_coroutines_jetpack_project.data.model.AllTouristAttractions
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,7 +9,7 @@ interface TouristAttractionsApiService {
     suspend fun getAllTouristAttractions(
         @Query("lang") lang: String,
         @Query("page") page: String
-    )
+    ): AllTouristAttractions
 
     // another APIs
 }
--- app/src/main/java/com/shrc/duytoanng/taipeitour_kotlin_mvvm_coroutines_jetpack_project/repository/ITouristAttractionRepository.kt
+++ app/src/main/java/com/shrc/duytoanng/taipeitour_kotlin_mvvm_coroutines_jetpack_project/repository/ITouristAttractionRepository.kt
@@ -1,4 +1,10 @@
 package com.shrc.duytoanng.taipeitour_kotlin_mvvm_coroutines_jetpack_project.repository
 
+import com.shrc.duytoanng.taipeitour_kotlin_mvvm_coroutines_jetpack_project.data.model.AllTouristAttractions
+import com.shrc.duytoanng.taipeitour_kotlin_mvvm_coroutines_jetpack_project.utils.network.DataState
+import kotlinx.coroutines.flow.Flow
+
 interface ITouristAttractionRepository {
+    suspend fun getAllTouristAttractions(language: String, page: Int): Flow<DataState<AllTouristAttractions>>
+    // another functions
 }
